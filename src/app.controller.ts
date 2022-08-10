69cf30449d7dc7ec6468474386d67e01d36d99c3
Edwin Mambo <educhinski650@gmail.com>
2022-08-10 18:21:59 +0300

    Setup Views
    Created routes and hbs files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('login')
+  @Render('login')
+  login() {
+    return;
+  }
+
+  @Get('signup')
+  @Render('signup')
+  signup() {
+    return;
   }
 }
