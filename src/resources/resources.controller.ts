b32baf7ce44b26490f8951a1f58ff9080cf243cf
Edwin Mambo <educhinski650@gmail.com>
2022-08-10 22:47:17 +0300

    Authentication Strategy
    Integrated our authentication strategy into the endpoints

--- src/resources/resources.controller.ts
+++ src/resources/resources.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { FirebaseAuthGuard } from 'src/firebase/firebase-auth.guard';
 import { ResourcesService } from './resources.service';
 
 @Controller('resources')
@@ -6,6 +7,7 @@ export class ResourcesController {
   constructor(private readonly resourcesService: ResourcesService) {}
 
   @Get('')
+  @UseGuards(FirebaseAuthGuard)
   getAll() {
     return this.resourcesService.getAll();
   }
