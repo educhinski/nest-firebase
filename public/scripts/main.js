a0551ab529f876b804a9d5f24f17f031f8bb7a90
Edwin Mambo <educhinski650@gmail.com>
2022-08-10 22:00:17 +0300

    Firebase Client
    Connected the application with the firebase
    Implemented methods to handle users signup and login

---
+++ public/scripts/main.js
@@ -0,0 +1,30 @@
+const quotes = document.getElementById('quotes');
+const error = document.getElementById('error');
+
+function initializeApp() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCsF7ONA5Sn2bSL5t_WYBeyalf3kTBL40Q',
+    authDomain: 'fir-auth-2c6f0.firebaseapp.com',
+    projectId: 'fir-auth-2c6f0',
+    storageBucket: 'fir-auth-2c6f0.appspot.com',
+    messagingSenderId: '809073435692',
+    appId: '1:809073435692:web:e4f1d0f6d94914b63c1d89',
+    measurementId: 'G-B1YGEV7Q6J',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  firebase.initializeApp(firebaseConfig);
+
+  const displayQuotes = (allQuotes) => {
+    let html = '';
+    for (const quote in allQuotes) {
+      html += `<blockquote class="wp-block-quote">
+              <p>${quote.quote}</p><cite>${quote.character}</cite>
+            </blockquote>`;
+    }
+    return html;
+  };
+}
