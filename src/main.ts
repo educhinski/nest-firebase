d5bb6da575485326904069b58b47e1dc3a90e549
Edwin Mambo <educhinski650@gmail.com>
2022-08-10 17:37:55 +0300

    Template Engine Setup
    Installed and setup handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const logger = new Logger('App');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+  app.setViewEngine('hbs');
+  app.set('view options', { layout: 'main' });
   await app.listen(3000);
+  logger.log('Application started on port 3000');
 }
 bootstrap();
